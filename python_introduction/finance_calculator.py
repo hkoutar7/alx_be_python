f83ed31c8562bfc77a8031794bb4a586fe6fb1a5
hkoutar7 <ayanokoji09opp@gmail.com>
2024-09-15 16:59:41 +0100

    Task 6 Have been completed

--- python_introduction/finance_calculator.py
+++ python_introduction/finance_calculator.py
@@ -1,11 +1,10 @@
-INTEREST = 0.05
-
 monthly_income = int(input("Enter your monthly income: "))
 monthly_expenses = int(input("Enter your total monthly expenses:"))
 
 mothly_savings = monthly_income - monthly_expenses
 
-yearly_savings = int(mothly_savings * 12 * (1 + INTEREST))
+projected_savings = mothly_savings* 12 + (mothly_savings * 12 * 0.05)
+
 
 print(f"Your monthly savings are ${mothly_savings}.")
-print(f"Projected savings after one year, with interest, is: ${yearly_savings}.")
+print(f"Projected savings after one year, with interest, is: ${int(projected_savings)}.")
