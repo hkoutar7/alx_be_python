52530c6bf40c76b9345b283beaa6d5949271fa36
hkoutar7 <ayanokoji09opp@gmail.com>
2024-09-15 16:33:46 +0100

    Task 1 Have been completed

---
+++ python_introduction/basic_operations.py
@@ -0,0 +1,9 @@
+number1, number2 = 10, 5
+
+sum = number1 + number2
+diff = number1 - number2
+mult = number1 * number2
+
+print("Addition of %d and %d is %d" % (number1, number2, sum))
+print("Subtraction of %d and %d is %d" % (number1, number2, diff))
+print("Multiplication of %d and %d is %d" % (number1, number2, mult))
